111001084509dbd737ee9a60ca5a64a37086d040
tahseen31 <63615375+tahseen31@users.noreply.github.com>
2021-05-12 23:27:12 -0400

    About Button displays Info

--- app/src/main/java/edu/neu/madcourse/numad21su_tahseenhameed/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_tahseenhameed/MainActivity.java
@@ -2,13 +2,32 @@ package edu.neu.madcourse.numad21su_tahseenhameed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button aboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hi, I am Tahseen!\n" + " you can reach me at hameed.t@northeastern.edu";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        });
     }
 }
